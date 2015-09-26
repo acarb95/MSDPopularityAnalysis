86f9dd378bbc2041ec98e5ecadf631c25915857d
Amanda Carbonari <acarb95@gmail.com>
2015-09-26 14:01:23 -0600

    Added project to do specific country

---
+++ GetTop40CountrySpecific/README.md
@@ -0,0 +1,2 @@
+To run:
+top40.jar TopNRunner s3://bucketname/input s3://bucketname/output s3://bucketname/lookupTable.txt
---
+++ GetTop40CountrySpecific/src/TopNReducer.java
@@ -0,0 +1,42 @@
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.output.MultipleOutputs;
+
+public class TopNReducer extends Reducer<Text, SongWritable, Text, Text> {
+	private MultipleOutputs<Text, Text> mos;
+	
+	public void setup(Context context) throws IOException, InterruptedException {
+		super.setup(context);
+		mos = new MultipleOutputs<Text, Text>(context);
+	}
+	
+	public void reduce(Text key, Iterable<SongWritable> values, Context context) throws IOException, InterruptedException {
+		ArrayList<SongWritable> list = new ArrayList<SongWritable>();
+		
+		for (SongWritable val : values) {
+			SongWritable song = new SongWritable(val);
+			list.add(song);
+		}
+		
+		Collections.sort(list);
+		
+		int length = list.size();
+		
+		if (length > 40){
+			length = 40;
+		}
+		
+		for (int i = 0; i < length; i++) {
+			mos.write(new Text(list.get(i).identifyingData), new Text(list.get(i).getFeatureData()), key + "Top40Songs");
+		}
+	}
+	
+	public void cleanup(Context context) throws IOException, InterruptedException {
+		super.cleanup(context);
+		mos.close();
+	}
+}
