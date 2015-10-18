9bec9ec148e3ea2b9b705507aa065b30423cc3fb
Amanda Carbonari <Amanda.Carbonari@logrhythm.com>
2015-10-18 10:27:41 -0600

    Added something to run the US GB without 2D arrays

---
+++ GetTop100USGB/src/TopNRunner.java
@@ -0,0 +1,35 @@
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.output.LazyOutputFormat;
+import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
+
+
+public class TopNRunner {
+	public static void main(String[] args) throws IOException, ClassNotFoundException, InterruptedException, URISyntaxException {
+		String s3file = args[2];
+		Configuration conf = new Configuration();
+		Job job = Job.getInstance(conf, "Top 100 Per Country");
+		job.setJarByClass(TopNRunner.class);
+		job.setMapperClass(TopNMapper.class);
+		job.setReducerClass(TopNReducer.class);
+		job.setCombinerClass(TopNCombiner.class);
+		job.setMapOutputKeyClass(Text.class);
+		job.setMapOutputValueClass(SongWritable.class);
+		job.setOutputKeyClass(Text.class);
+		job.setOutputValueClass(Text.class);
+		job.addCacheFile(new URI(s3file + "#theFile1"));
+		LazyOutputFormat.setOutputFormatClass(job, TextOutputFormat.class);
+		FileInputFormat.addInputPath(job, new Path(args[0]));
+		FileInputFormat.setInputDirRecursive(job, true);
+		FileOutputFormat.setOutputPath(job, new Path(args[1]));
+		job.waitForCompletion(true);
+	}
+}
